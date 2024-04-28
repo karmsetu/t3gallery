582806b6b2716b0d478727f85d9289536b8fa2de
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2024-04-29 00:13:20 +0530

    modal UI clean up

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,6 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
-  return <img src={image?.url} alt={image?.name} className="h-96 w-96" />;
+  return (
+    <div className="flex h-full w-full min-w-0 bg-green-500">
+      <div className="flex-shrink items-center justify-center">
+        <img src={image?.url} alt={image?.name} className=" object-contain" />
+      </div>
+      <div className="flex w-48 flex-shrink flex-col border-l">
+        <div className="text-xl font-bold ">{image?.name}</div>
+      </div>
+    </div>
+  );
 }
